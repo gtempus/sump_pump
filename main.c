75cfef37a5e55d888b9410b08dcbba783f4c8e4a
Gary Tempus <gtempus@codegaloot.com>
2016-03-23 23:35:01 -0400

    initial add.

---
+++ main.c
@@ -0,0 +1,18 @@
+
+#include "main.h" 
+
+// -------- Global Variables --------- //
+
+// -------- Functions --------- //
+
+int main(void) {
+  // -------- Inits --------- //
+
+  // ------ Event loop ------ //
+  while (1) {
+
+
+
+  }                                                  /* End event loop */
+  return (0);                            /* This line is never reached */
+}
---
+++ main.h
@@ -0,0 +1,13 @@
+
+// Standard AVR includes
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+#include <avr/eeprom.h>
+#include <avr/pgmspace.h>
+#include <avr/sleep.h>
+#include <avr/wdt.h>
+#include <avr/power.h>
+
+// Standard includes
+#include <stdlib.h>
